7ce20e286558b2d55a87230252672534b3d44138
Rio Edwards <rioredwards@gmail.com>
2024-05-24 15:15:48 -0700

    Redo home page to be HTML, CSS and JS. and look good

---
+++ public/script.js
@@ -0,0 +1,31 @@
+const form = document.getElementById("translate-form");
+const textEl = document.getElementById("text");
+const languageEl = document.getElementById("language");
+const result = document.getElementById("result");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const text = textEl.value;
+  const language = languageEl.value;
+
+  fetch("/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ text, language }),
+  })
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Something went wrong...");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      result.innerText = JSON.stringify(data, null, 2);
+    })
+    .catch((error) => {
+      console.warn(error);
+    });
+});
--- server.js
+++ server.js
@@ -25,25 +25,7 @@ async function translateText(text, target) {
   return translations;
 }
 
-app.get("/", async (_, res) => {
-  const greeting = `Hello from the Translate API! 👋
-    You sent a GET request, but you will need to send a POST request to use this API.
-
-    Please send a POST request that includes some text to translate and language to translate to in the body of the request. 
-
-    Your fetch should look like this: 
-    fetch(INSERT_URL_HERE, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        text: INSERT_TEXT_HERE,
-        language: INSERT_LANGUAGE_HERE,
-      }),
-    })`;
-  res.send(greeting);
-});
+app.use(express.static("public"));
 
 app.post("/", express.json(), async (req, res) => {
   const text = req.body.text;
@@ -51,7 +33,7 @@ app.post("/", express.json(), async (req, res) => {
 
   try {
     const translateRes = await translateText(text, language);
-    res.send(translateRes);
+    res.json(translateRes);
   } catch (error) {
     const status = error.code || 500;
     res.status(status).send(error.message);
